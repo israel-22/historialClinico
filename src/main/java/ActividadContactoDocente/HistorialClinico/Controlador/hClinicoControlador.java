1d90cd2d74f204040b43663c70e1fe4b5e902f9e
Santos-de-Israel <144865373+Santos-de-Israel@users.noreply.github.com>
2025-01-26 07:46:27 -0500

    tercer intento exitoso

--- src/main/java/ActividadContactoDocente/HistorialClinico/Controlador/hClinicoControlador.java
+++ src/main/java/ActividadContactoDocente/HistorialClinico/Controlador/hClinicoControlador.java
@@ -1,28 +1,22 @@
 package ActividadContactoDocente.HistorialClinico.Controlador;
 
-
 import ActividadContactoDocente.HistorialClinico.Entidad.hClinico;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.Arrays;
 import java.util.List;
-
-
 
 @Controller
 public class hClinicoControlador {
 
-    @GetMapping("/historialClinico")
-    public String historial( Model model) {
-        hClinico hClinico = new hClinico();
+    @GetMapping("/historialesClinicos")
+    public String historial(Model model) {
+        hClinico hClinico = new hClinico(); // Crear una nueva instancia de hClinico
         List<String> ListaPacientes = Arrays.asList("Paciente 1", "Paciente 2", "Paciente 3");
-        model.addAttribute("listados", ListaPacientes);
-        return "pages/historialesClinicos";
-
+        model.addAttribute("listados", ListaPacientes); // Lista de pacientes
+        model.addAttribute("hClinico", hClinico); // Añadir el objeto hClinico al modelo
+        return "pages/historialesClinicos"; // Devolver la vista
     }
-
-
-
-
 }
--- src/main/java/ActividadContactoDocente/HistorialClinico/Entidad/hClinico.java
+++ src/main/java/ActividadContactoDocente/HistorialClinico/Entidad/hClinico.java
@@ -14,7 +14,6 @@ import org.springframework.format.annotation.NumberFormat;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 public class hClinico {
 
     @NotBlank
@@ -81,6 +80,133 @@ private String address;
 
     /*Al no funcionar bien @Data seprosedio a colocar los Getters y Setters de manera manual*/
 
+    public @NotBlank @Pattern(regexp = "^[0-9]{10}$") String getCedulaIdentidad() {
+        return cedulaIdentidad;
+    }
+
+    public void setCedulaIdentidad(@NotBlank @Pattern(regexp = "^[0-9]{10}$") String cedulaIdentidad) {
+        this.cedulaIdentidad = cedulaIdentidad;
+    }
+
+    public @Size(min = 3, max = 50) String getName() {
+        return name;
+    }
+
+    public void setName(@Size(min = 3, max = 50) String name) {
+        this.name = name;
+    }
+
+    public @NotBlank @Size(min = 3, max = 50) String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@NotBlank @Size(min = 3, max = 50) String lastName) {
+        this.lastName = lastName;
+    }
+
+    public @NotBlank String getSeguroSocial() {
+        return SeguroSocial;
+    }
+
+    public void setSeguroSocial(@NotBlank String seguroSocial) {
+        SeguroSocial = seguroSocial;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public @NotBlank @Size(min = 3, max = 50) String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(@NotBlank @Size(min = 3, max = 50) String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public @NotBlank @Size(min = 3, max = 50) String getAge() {
+        return age;
+    }
+
+    public void setAge(@NotBlank @Size(min = 3, max = 50) String age) {
+        this.age = age;
+    }
+
+    public @NotBlank @Size(min = 3, max = 50) String getHeight() {
+        return height;
+    }
+
+    public void setHeight(@NotBlank @Size(min = 3, max = 50) String height) {
+        this.height = height;
+    }
+
+    public @NotBlank @Size(min = 3, max = 50) String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(@NotBlank @Size(min = 3, max = 50) String weight) {
+        this.weight = weight;
+    }
+
+    public @NotBlank @Email(message = "El correo es inválido") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank @Email(message = "El correo es inválido") String email) {
+        this.email = email;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public @NotBlank @Pattern(regexp = "^[0-9]{10}$") String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(@NotBlank @Pattern(regexp = "^[0-9]{10}$") String phone) {
+        this.phone = phone;
+    }
+
+    public @NotBlank String getAddress() {
+        return address;
+    }
+
+    public void setAddress(@NotBlank String address) {
+        this.address = address;
+    }
+
+    public @NotBlank String getGender() {
+        return gender;
+    }
+
+    public void setGender(@NotBlank String gender) {
+        this.gender = gender;
+    }
+
+    public @Size(min = 10, max = 150) String getCountry() {
+        return country;
+    }
+
+    public void setCountry(@Size(min = 10, max = 150) String country) {
+        this.country = country;
+    }
+
+    public @Size(min = 10, max = 150) String getCity() {
+        return city;
+    }
+
+    public void setCity(@Size(min = 10, max = 150) String city) {
+        this.city = city;
+    }
 }
 
 
